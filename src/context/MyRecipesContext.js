81fc2db3d0793e0cf0e9a8312dbb1ad1349ab73d
David <ddropping@gmail.com>
2020-10-20 11:45:45 -0700

    Create delete recipe context flow

--- src/context/MyRecipesContext.js
+++ src/context/MyRecipesContext.js
@@ -3,7 +3,15 @@ import createDataContext from "./createDataContext";
 const recipesReducer = (state, action) => {
   switch (action.type) {
     case "ADD_RECIPE":
-      return [...state, { title: `Recipe ${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999999),
+          title: `Recipe ${state.length + 1}`,
+        },
+      ];
+    case "DELETE_RECIPE":
+      return state.filter((recipe) => recipe.id !== payload);
     default:
       return state;
   }
@@ -15,8 +23,14 @@ const addRecipe = (dispatch) => {
   };
 };
 
+const deleteRecipe = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "DELETE_RECIPE", payload: id });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   recipesReducer,
-  { addRecipe },
+  { addRecipe, deleteRecipe },
   []
 );
