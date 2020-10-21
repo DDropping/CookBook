6c35b30e3bbcef93008c296f832715f24e838eb4
David <ddropping@gmail.com>
2020-10-21 16:05:32 -0700

    Create api folder

---
+++ api/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+const router = express.Router();
+
+// @route   POST api/auth/signup
+// @desc    Register new user account
+// @access  Public
+router.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = new User({ email, password });
+
+    await user.save();
+    res.send("post request to signup");
+  } catch (err) {
+    res.send("Could Not Register User");
+    console.log(err.message);
+  }
+});
+
+module.exports = router;
