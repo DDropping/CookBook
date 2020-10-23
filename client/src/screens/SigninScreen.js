62d668990b4bc388d007a7feccbfe57e87188050
David <ddropping@gmail.com>
2020-10-23 15:22:39 -0700

    Create ui for signin andsignup screens

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -1,26 +1,55 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+
+import Spacer from "../components/Spacer";
 
 const SigninScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>SigninScreen</Text>
+    <View style={styles.container}>
+      <Text h3 style={{ textAlign: "center" }}>
+        Sign In
+      </Text>
+      <Spacer>
+        <Input label="Email" />
+      </Spacer>
+      <Spacer>
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Button
+          title="Sign In"
+          onPress={() => {
+            navigation.navigate("Signin");
+          }}
+        />
+      </Spacer>
+      <Spacer>
+        <Text style={{ textAlign: "center" }}>- or -</Text>
+      </Spacer>
       <Button
-        title="go to signup"
+        title="Click Here To Sign Up"
+        type="clear"
         onPress={() => {
           navigation.navigate("Signup");
-        }}
-      />
-      <Button
-        title="go to main flow"
-        onPress={() => {
-          navigation.navigate("mainFlow");
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 
 export default SigninScreen;
--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -1,20 +1,48 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>SignupScreen</Text>
-      <Button
-        title="go to signin"
-        onPress={() => {
-          navigation.navigate("Signin");
-        }}
-      />
+    <View style={styles.container}>
+      <Text h3 style={{ textAlign: "center" }}>
+        Sign Up
+      </Text>
+      <Spacer>
+        <Input label="Email" />
+      </Spacer>
+      <Spacer>
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Input label="Confirm Password" />
+      </Spacer>
+      <Spacer>
+        <Button
+          title="Sign Up"
+          onPress={() => {
+            navigation.navigate("Signin");
+          }}
+        />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 
 export default SignupScreen;
