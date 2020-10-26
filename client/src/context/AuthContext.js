e09c55abec11bd3bf24b562f890ad64090b30ccc
David <ddropping@gmail.com>
2020-10-26 15:26:25 -0700

    Add asyncStorage to hold jwt

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,5 +1,7 @@
 import createDataContext from "./createDataContext";
+import AsyncStorage from "@react-native-community/async-storage";
 
+import { navigate } from "../navigationRef";
 import cookbookApi from "../api/cookbookApi";
 
 const authReducer = (state, action) => {
@@ -9,41 +11,49 @@ const authReducer = (state, action) => {
         ...state,
         errorMessage: action.payload,
       };
+    case "signup":
+      return {
+        ...state,
+        token: action.payload,
+        errorMessage: "",
+      };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      const response = await cookbookApi.post("/api/auth/signup", {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (err) {
-      dispatch({ type: "add_error", payload: "Something went wrong" });
-      console.log(err.message);
-    }
-  };
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    //create new user and return token
+    const response = await cookbookApi.post("/api/auth/signup", {
+      email,
+      password,
+    });
+    //store token in async storage
+    await AsyncStorage.setItem("token", response.data.token);
+    //store token in context
+    dispatch({ type: "signup", payload: response.data.token });
+    //navigate to main flow
+    navigate("mainFlow");
+  } catch (err) {
+    dispatch({ type: "add_error", payload: "Something went wrong" });
+    console.log(err.message);
+  }
 };
 
-const signin = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      console.log(email);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+const signin = (dispatch) => async ({ email, password }) => {
+  try {
+    console.log(email);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, signin },
   {
-    isAuthenticated: false,
+    token: null,
     user: null,
     errorMessage: "",
   }
